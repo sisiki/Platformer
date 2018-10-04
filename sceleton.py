28ca7a987ea2eeca2bcdea235efff53629d38bb3
sisiki <228baby777@gmail.com>
2018-10-04 18:04:05 +0300

    full sceleton

--- sceleton.py
+++ sceleton.py
@@ -1,2 +1,23 @@
 import pygame
+BLACK = (0,0,0)
 pygame. init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Platformer Game")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pass
+
+
+screen.fill(BLACK)
+
+pygame.quit()
